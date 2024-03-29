3e57357a477358c94176358b1ce7ce5076282b73
Raafey Raza <Raafeyraza1@gmail.com>
2024-03-29 17:52:42 +0500

    updated a lot of stuff

--- script.js
+++ script.js
@@ -1,118 +1,155 @@
 particlesJS("particles-js", {
-    "particles": {
-      "number": {
-        "value": 200,
-        "density": {
-          "enable": true,
-          "value_area": 800
-        }
-      },
-      "color": {
-        "value": "#ffffff" // White particles
-      },
-      "shape": {
-        "type": "circle",
-        "stroke": {
-          "width": 0,
-          "color": "#000000"
-        },
-        "polygon": {
-          "nb_sides": 5
-        }
-      },
-      "opacity": {
-        "value": 0.8, // Increased opacity
-        "random": false,
-        "anim": {
-          "enable": false,
-          "speed": 1,
-          "opacity_min": 0.1,
-          "sync": false
-        }
-      },
-      "size": {
-        "value": 1, // Fixed size value
-        "random": false, // Disable random size variation
-        "anim": {
-          "enable": false,
-          "speed": 40,
-          "size_min": 0.1,
-          "sync": false
-        }
-      },
-      "line_linked": {
+  "particles": {
+    "number": {
+      "value": 200,
+      "density": {
         "enable": true,
-        "distance": 150,
-        "color": "#ffffff", // White connections
-        "opacity": 0.4,
-        "width": 1
-      },
-      "move": {
-        "enable": true,
-        "speed": 2, // Decreased movement speed
-        "direction": "none",
-        "random": false,
-        "straight": false,
-        "out_mode": "out",
-        "bounce": false
+        "value_area": 800
       }
     },
-    "interactivity": {
-      "detect_on": "window",
-      "events": {
-        "onhover": {
-          "enable": true,
-          "mode": "grab" // Changed on hover mode to grab
-        },
-        "onclick": {
-          "enable": true,
-          "mode": "bubble" // Changed on click mode to bubble
-        },
-        "resize": true
+    "color": {
+      "value": "#ffffff" // White particles
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
       },
-      "modes": {
-        "grab": {
-          "distance": 140,
-          "line_linked": {
-            "opacity": 1
-          }
-        },
-        "bubble": {
-          "distance": 100,
-          "size": 4,
-          "duration": 0.2,
-          "opacity": 8,
-          "speed": 3
-        },
-        "repulse": {
-          "distance": 200,
-          "duration": 0.4
-        },
-        "push": {
-          "particles_nb": 4
-        },
-        "remove": {
-          "particles_nb": 2
-        }
+      "polygon": {
+        "nb_sides": 5
       }
     },
-    "retina_detect": true
-  });
-  
-  //transform the images icons
-  document.getElementById("icon").addEventListener("click", function() {
-    var image = document.getElementById("image");
-    if (image.style.transform === "rotateY(0deg)") {
-      image.style.transform = "rotateY(180deg)";
-    } else {
-      image.style.transform = "rotateY(0deg)";
+    "opacity": {
+      "value": 0.8, // Increased opacity
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 1, // Fixed size value
+      "random": false, // Disable random size variation
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff", // White connections
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 2, // Decreased movement speed
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false
     }
-  });
-  document.getElementById("image").addEventListener("click", function() {
-    var icon = document.getElementById("image");
-    if (icon.style.transform === "rotateY(0deg)") {
-       icon.style.transform = "rotateY(180deg)";
-    } else {
-       icon.style.transform = "rotateY(0deg)";
+  },
+  "interactivity": {
+    "detect_on": "window",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "grab" // Changed on hover mode to grab
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "bubble" // Changed on click mode to bubble
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 140,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 100,
+        "size": 7,
+        "duration": 0.2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
     }
-   });
+  },
+  "retina_detect": true
+});
+
+// transform the images icons
+document.getElementById("icon").addEventListener("click", function () {
+  var image = document.getElementById("image");
+  if (image.style.transform === "rotateY(0deg)") {
+    image.style.transform = "rotateY(180deg)";
+  } else {
+    image.style.transform = "rotateY(0deg)";
+  }
+});
+document.getElementById("image").addEventListener("click", function () {
+  var icon = document.getElementById("image");
+  if (icon.style.transform === "rotateY(0deg)") {
+    icon.style.transform = "rotateY(180deg)";
+  } else {
+    icon.style.transform = "rotateY(0deg)";
+  }
+});
+
+// curtain navigation js
+/* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+/* Smooth scrolling for all browsers */
+$(document).ready(function () {
+  // Add smooth scrolling to all links
+  $("a").on('click', function (event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function () {
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
